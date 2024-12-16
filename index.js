00ec4ba218c2def8478a7ce39c8013fbaf7e06ab
techyyhimanshu <158696334+techyyhimanshu@users.noreply.github.com>
2024-12-16 17:42:54 +0530

    removed hello message

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ app.use(cookieParser())
 app.use(express.json({
     limit: "50mb"
 }))
-app.use("/", (req, res) => {
-    return res.status(200).send("hello from chatty server")
-})
 app.use("/api", routes)
 const PORT = process.env.PORT
 server.listen(PORT, () => {
